81f0e93610f87cf909d451c8e002356a6bcc9fb6
Connor Ruggles <conruggles@gmail.com>
2022-12-09 21:38:47 -0600

    day 8 part 2

--- day8/__main__.py
+++ day8/__main__.py
@@ -25,6 +25,32 @@ def can_be_seen(tree, north, south, east, west):
     )
 
 
+def takewhile_inclusive(predicate, it):
+    """
+    needed to grab the element that takewhile
+    drops - defined my own function to do it
+    """
+    for x in it:
+        if predicate(x):
+            yield x
+        else:
+            yield x
+            break
+
+
+def view_score(tree, north, south, east, west):
+    right = [t for t in takewhile_inclusive(lambda t: t < tree, north)]
+    left = [t for t in takewhile_inclusive(lambda t: t < tree, south)]
+    down = [t for t in takewhile_inclusive(lambda t: t < tree, east)]
+    up = [t for t in takewhile_inclusive(lambda t: t < tree, west)]
+    score = 1
+    score *= len(right) if right else 1
+    score *= len(up) if up else 1
+    score *= len(left) if left else 1
+    score *= len(down) if down else 1
+    return score
+
+
 def part_one(filename):
     with open(filename) as f:
         tree_grid = fill_out_grid([l.strip() for l in f.readlines()])
@@ -35,10 +61,10 @@ def part_one(filename):
             for j in range(1, len(tree_grid[i]) - 1):
                 if can_be_seen(
                     tree_grid[i][j],
-                    [tree_grid[n][j] for n in range(0, i)],
+                    reversed([tree_grid[n][j] for n in range(0, i)]),
                     [tree_grid[s][j] for s in range(i + 1, len(tree_grid))],
-                    [tree_grid[i][e] for e in range(0, j)],
                     [tree_grid[i][w] for w in range(j + 1, len(tree_grid[i]))],
+                    reversed([tree_grid[i][e] for e in range(0, j)]),
                 ):
                     num_visible += 1
 
@@ -47,12 +73,23 @@ def part_one(filename):
 
 def part_two(filename):
     with open(filename) as f:
-        lines = [l.strip() for l in f.readlines()]
-        pass
+        tree_grid = fill_out_grid([l.strip() for l in f.readlines()])
+        scores = []
+        for i in range(1, len(tree_grid) - 1):
+            for j in range(1, len(tree_grid[i]) - 1):
+                scores.append(view_score(
+                    tree_grid[i][j],
+                    reversed([tree_grid[n][j] for n in range(0, i)]),
+                    [tree_grid[s][j] for s in range(i + 1, len(tree_grid))],
+                    [tree_grid[i][w] for w in range(j + 1, len(tree_grid[i]))],
+                    reversed([tree_grid[i][e] for e in range(0, j)]),
+                ))
+    
+    return max(scores)
 
 
 if __name__ == "__main__":
     cur_dir = __file__.split("/")[0]
     assert part_one(f"{cur_dir}/test.txt") == 21
-    # assert part_two(f"{cur_dir}/test.txt") == 0
+    assert part_two(f"{cur_dir}/test.txt") == 8
     print(part_one(f"{cur_dir}/input.txt"), part_two(f"{cur_dir}/input.txt"))
